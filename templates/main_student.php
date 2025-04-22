98352ffb602ff1e5b7a7d44ef9c74552d39e8589
Dennis Wolfram <post@dennis-wolfram.de>
2025-04-22 17:00:24 +0200

    Update main_student.php
    Short-Tags entfernt:
    
    Alle Short-Tags wurden durch die vollständige PHP-Notation <?php ersetzt.
    Sicherheitsmaßnahmen:
    
    Ersetzt htmlReady() durch htmlspecialchars() für Benutzerinhalte, um XSS-Angriffe zu verhindern.
    Formatierung und Lesbarkeit:
    
    Der Code wurde besser strukturiert und mit Kommentaren versehen, um die Lesbarkeit und Wartbarkeit zu verbessern.
    Korrekte Typisierung und Konsistenz:
    
    Überprüfung und Ausgabe von Werten (z. B. Gesamtnote) mit sicherer und sauberer Typisierung.

--- templates/main_student.php
+++ templates/main_student.php
@@ -1,35 +1,50 @@
-<? if(empty($_SESSION['messages'])): ?>
+<?php if (empty($_SESSION['messages'])): ?>
     <div style="margin:20px;">
-        <div style="font-size:120%;font-weight:bold;"><?=$plugin->_("Noten für:") . '&nbsp;' . htmlReady($fsz->members[$user_id]['fullname'])?></div>
-        <? if ($plugin->fsz): ?>
-            <div style="font-size:120%;font-weight:bold;"><?=$plugin->_("Niveau:") . '&nbsp;' . htmlReady($fsz->getValueNiveau($user_id))?></div>
-        <? endif ?>
+        <!-- Header: Noten für -->
+        <div style="font-size:120%; font-weight:bold;">
+            <?php echo $plugin->_("Noten für:") . '&nbsp;' . htmlspecialchars($fsz->members[$user_id]['fullname']); ?>
+        </div>
+
+        <!-- Optional: Niveau -->
+        <?php if ($plugin->fsz): ?>
+            <div style="font-size:120%; font-weight:bold;">
+                <?php echo $plugin->_("Niveau:") . '&nbsp;' . htmlspecialchars($fsz->getValueNiveau($user_id)); ?>
+            </div>
+        <?php endif; ?>
+
+        <!-- Einzelnoten -->
         <ol>
-            <? foreach ($fsz->config->marks as $mark_id => $mark_data): ?>
-                <li >
+            <?php foreach ($fsz->config->marks as $mark_id => $mark_data): ?>
+                <li>
                     <span style="width:200px;">
-                        <?=htmlReady(($mark_data['desc'] ? $mark_data['desc'] : $plugin->_("Einzelnote")))?>
+                        <?php echo htmlspecialchars($mark_data['desc'] ?: $plugin->_("Einzelnote")); ?>
                         &nbsp;
-                        (<?=htmlReady($mark_data['weight'])?>%):
+                        (<?php echo htmlspecialchars($mark_data['weight']); ?>%):
                     </span>
-                    <?=$fsz->getValue($user_id, $mark_id, 'value')?>
+                    <?php echo htmlspecialchars($fsz->getValue($user_id, $mark_id, 'value')); ?>
                 </li>
-            <? endforeach ?>
+            <?php endforeach; ?>
+
+            <!-- Gesamtnote oder Erfolg -->
             <li style="list-style-type:none; margin-top:20px;">
-                <? if ($fsz->getValueErfolg($user_id)): ?>
+                <?php if ($fsz->getValueErfolg($user_id)): ?>
                     <span style="width:200px;">
-                        <?=$plugin->_("Mit Erfolg bestanden") ?>
+                        <?php echo $plugin->_("Mit Erfolg bestanden"); ?>
                     </span>
-                <? elseif ($fsz->getFinalGrade($user_id)): ?>
+                <?php elseif ($fsz->getFinalGrade($user_id)): ?>
                     <span style="width:200px;">
-                        <?=$plugin->_("Gesamtnote:") ?>
+                        <?php echo $plugin->_("Gesamtnote:"); ?>
                     </span>
-                    <?=($grade = round($fsz->getFinalGrade($user_id),1))?>
-                    &nbsp;<? if ($plugin->fsz): ?>
-                        (<?=$fsz->getGradeAsWord($grade)?>)
-                    <? endif ?>
-                <? endif ?>
+                    <?php
+                    $grade = round($fsz->getFinalGrade($user_id), 1);
+                    echo htmlspecialchars($grade);
+                    ?>
+                    &nbsp;
+                    <?php if ($plugin->fsz): ?>
+                        (<?php echo htmlspecialchars($fsz->getGradeAsWord($grade)); ?>)
+                    <?php endif; ?>
+                <?php endif; ?>
             </li>
         </ol>
     </div>
-<? endif ?>
+<?php endif; ?>
