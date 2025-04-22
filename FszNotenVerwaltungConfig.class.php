303140b7b55310ef125cad28621ab00223a83a93
Dennis Wolfram <post@dennis-wolfram.de>
2025-04-22 16:50:46 +0200

    Update FszNotenVerwaltungConfig.class.php
    Dynamische Eigenschaften entfernt:
    
    Alle Eigenschaften wie $seminar_id, $data, $marks und $conf wurden explizit deklariert.
    Typdeklarationen hinzugefügt:
    
    Parameter und Rückgabewerte sind mit Typangaben wie string, array und int versehen.
    Fehlertoleranz verbessert:
    
    Absicherung gegen potenziell undefinierte Array-Zugriffe durch Verwendung von ??.
    Robustheit erhöht:
    
    Die Methode deleteMark gibt immer einen Integer-Wert zurück, selbst wenn keine Änderungen vorgenommen werden.

--- FszNotenVerwaltungConfig.class.php
+++ FszNotenVerwaltungConfig.class.php
@@ -2,35 +2,40 @@
 
 class FszNotenVerwaltungConfig
 {
-    public $seminar_id;
-    public $data = array();
-    public $marks = array();
-    public $conf = array('NOTIFY_USER' => 0, 'VISIBLE' => 0, 'PART_VISIBLE' => 0, 'UNICERT' => 0);
+    public string $seminar_id;
+    public array $data = [];
+    public array $marks = [];
+    public array $conf = ['NOTIFY_USER' => 0, 'VISIBLE' => 0, 'PART_VISIBLE' => 0, 'UNICERT' => 0];
 
-    function __construct($seminar_id)
+    public function __construct(string $seminar_id)
     {
         $this->seminar_id = $seminar_id;
         $this->restore();
     }
 
-    function getNewId()
+    public function getNewId(): string
     {
-        return md5(uniqid('FszNotenVerwaltung',1));
+        return md5(uniqid('FszNotenVerwaltung', true));
     }
 
-    function restore()
+    public function restore(): void
     {
         $db = DbManager::get();
-        $this->data = array();
-        $this->marks = array();
-        $this->conf = array();
-        $this->data = $db->fetchGrouped("SELECT * FROM fsz_notenverwaltung_config WHERE seminar_id=? ORDER BY name", array($this->seminar_id));
-        foreach($this->data as $id => $data){
-            list($name, $bezug) = explode('-', $data['name']);
-            if($name == 'MARK'){
+        $this->data = [];
+        $this->marks = [];
+        $this->conf = [];
+
+        $this->data = $db->fetchGrouped(
+            "SELECT * FROM fsz_notenverwaltung_config WHERE seminar_id = ? ORDER BY name",
+            [$this->seminar_id]
+        );
+
+        foreach ($this->data as $id => $data) {
+            [$name, $bezug] = explode('-', $data['name'] ?? '-');
+            if ($name === 'MARK') {
                 $this->marks[$id]['desc'] = $data['value'];
                 $this->data[$id]['name'] = 'MARK';
-            } elseif($name == 'WEIGHT'){
+            } elseif ($name === 'WEIGHT') {
                 $this->marks[$bezug]['weight'] = $data['value'];
             } else {
                 $this->conf[$name] = $data['value'];
@@ -38,46 +43,59 @@ class FszNotenVerwaltungConfig
         }
     }
 
-    function store()
+    public function store(): int
     {
         $db = DbManager::get();
-        $positon = 0;
-        foreach($this->marks as $id => $data){
+        $position = 0;
+        $ret = 0;
+
+        foreach ($this->marks as $id => $data) {
             $this->data[$id]['id'] = $id;
             $this->data[$id]['seminar_id'] = $this->seminar_id;
-            $this->data[$id]['name'] = 'MARK-' . ++$positon;
+            $this->data[$id]['name'] = 'MARK-' . ++$position;
             $this->data[$id]['value'] = $data['desc'];
+
             $wid = md5('WEIGHT-' . $id);
             $this->data[$wid]['id'] = $wid;
             $this->data[$wid]['seminar_id'] = $this->seminar_id;
             $this->data[$wid]['name'] = 'WEIGHT-' . $id;
             $this->data[$wid]['value'] = $data['weight'];
         }
-        foreach($this->conf as $name => $value){
+
+        foreach ($this->conf as $name => $value) {
             $id = md5($name . $this->seminar_id);
             $this->data[$id]['id'] = $id;
             $this->data[$id]['seminar_id'] = $this->seminar_id;
             $this->data[$id]['name'] = $name;
             $this->data[$id]['value'] = (string)$value;
         }
-        foreach($this->data as $row){
+
+        foreach ($this->data as $row) {
             $ret += $db->execute(
-                "REPLACE INTO fsz_notenverwaltung_config (id,seminar_id,name,value) VALUES (?,?,?,?)",
-                array($row['id'],
-                      $row['seminar_id'],
-                      $row['name'],
-                      $row['value']));
+                "REPLACE INTO fsz_notenverwaltung_config (id, seminar_id, name, value) VALUES (?, ?, ?, ?)",
+                [
+                    $row['id'],
+                    $row['seminar_id'],
+                    $row['name'],
+                    $row['value']
+                ]
+            );
         }
+
         $this->restore();
         return $ret;
     }
 
-    function deleteMark($mark_id)
+    public function deleteMark(string $mark_id): int
     {
-        if(isset($this->marks[$mark_id])){
+        $ret = 0;
+        if (isset($this->marks[$mark_id])) {
             $db = DbManager::get();
-            $db->execute("DELETE FROM fsz_notenverwaltung_noten WHERE config_id = ?", array($mark_id));
-            $ret = $db->execute("DELETE FROM fsz_notenverwaltung_config WHERE id IN (?,?)", array($mark_id, md5('WEIGHT-' . $mark_id)));
+            $db->execute("DELETE FROM fsz_notenverwaltung_noten WHERE config_id = ?", [$mark_id]);
+            $ret = $db->execute(
+                "DELETE FROM fsz_notenverwaltung_config WHERE id IN (?, ?)",
+                [$mark_id, md5('WEIGHT-' . $mark_id)]
+            );
             $this->restore();
         }
         return $ret;
